75edc1eb0607453555649dd660d71df45cdf7ce1
Hoang-code <71956596+Hoang-code@users.noreply.github.com>
2020-10-25 20:27:24 +0700

    hoanthien

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function kiemtracannang(){
     let weight = Number(document.getElementById("weight").value)
     let height = Number(document.getElementById("height").value)
     let BMI = weight/(height*height)
-    document.getElementById("BMI_1").innerText = BMI
+    document.getElementById("BMI_1").attributes = BMI
     if (BMI < 18){
         document.getElementById("BMI").innerText = "Chỉ số cơ thể: Underweight"
     } else if (BMI < 25){
